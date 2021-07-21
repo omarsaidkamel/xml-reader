92a66f115a5f814650c1c8081c20b69e0894af34
Omar Saied <osk09876@gmail.com>
2021-07-21 16:48:16 +0200

    first add

---
+++ XML Reader/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace XML_Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.cddvvToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dsdsvToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 26;
+            this.listBox1.Location = new System.Drawing.Point(3, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(392, 134);
+            this.listBox1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSplitButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(397, 27);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cddvvToolStripMenuItem,
+            this.dsdsvToolStripMenuItem,
+            this.vToolStripMenuItem});
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(48, 24);
+            this.toolStripSplitButton1.Text = "File";
+            // 
+            // cddvvToolStripMenuItem
+            // 
+            this.cddvvToolStripMenuItem.Name = "cddvvToolStripMenuItem";
+            this.cddvvToolStripMenuItem.Size = new System.Drawing.Size(176, 24);
+            this.cddvvToolStripMenuItem.Text = "New XML File";
+            this.cddvvToolStripMenuItem.Click += new System.EventHandler(this.cddvvToolStripMenuItem_Click);
+            // 
+            // dsdsvToolStripMenuItem
+            // 
+            this.dsdsvToolStripMenuItem.Name = "dsdsvToolStripMenuItem";
+            this.dsdsvToolStripMenuItem.Size = new System.Drawing.Size(176, 24);
+            this.dsdsvToolStripMenuItem.Text = "Clear The Page";
+            this.dsdsvToolStripMenuItem.Click += new System.EventHandler(this.dsdsvToolStripMenuItem_Click);
+            // 
+            // vToolStripMenuItem
+            // 
+            this.vToolStripMenuItem.Name = "vToolStripMenuItem";
+            this.vToolStripMenuItem.Size = new System.Drawing.Size(176, 24);
+            this.vToolStripMenuItem.Text = "Exit";
+            this.vToolStripMenuItem.Click += new System.EventHandler(this.vToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 167);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(395, 209);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 373);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XML Reader";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem cddvvToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dsdsvToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ XML Reader/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+namespace XML_Reader
+{
+    public partial class Form1 : Form
+    {
+        string line = string.Empty;
+        int line_number = 1;
+        int line_to_edit = 5;
+        public Form1()
+        {
+         InitializeComponent();
+        }
+        private void cddvvToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(@"D:\programing\XML Reader\XML Reader\Refined_RNA EF.XML.txt", FileMode.Open);
+            StreamWriter writer = new StreamWriter(@"D:\programing\XML Reader\XML Reader\Refined_RNA EF.XML.txt");
+            while (line  != null)
+            {
+                if (line_number == line_to_edit)
+                {
+                    writer.WriteLine(line);
+
+                }
+
+                line_number++;
+            }
+
+            // var lineCount = File.ReadLines(@"D:\programing\XML Reader\XML Reader\Refined_RNA EF.XML 1.txt").Count();
+           // richTextBox1.Text =Convert.ToString( lineCount);
+        }
+        private void dsdsvToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();        
+        }
+        private void vToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+      }
+}
